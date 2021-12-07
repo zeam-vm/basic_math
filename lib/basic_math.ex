0fe4b78588f017fce6f8a82df1d4379f852f3ad0
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-07 10:54:06 +0900

    remove hello

--- lib/basic_math.ex
+++ lib/basic_math.ex
@@ -2,17 +2,4 @@ defmodule BasicMath do
   @moduledoc """
   Documentation for `BasicMath`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BasicMath.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/basic_math_test.exs
+++ test/basic_math_test.exs
@@ -1,8 +1,4 @@
 defmodule BasicMathTest do
   use ExUnit.Case
   doctest BasicMath
-
-  test "greets the world" do
-    assert BasicMath.hello() == :world
-  end
 end
