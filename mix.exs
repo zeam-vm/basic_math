2b3cb2c7528e553a2a5f1fdd1867bf877b1ece6f
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-07 11:11:59 +0900

    make version to bee 0.1.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BasicMath.MixProject do
   def project do
     [
       app: :basic_math,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
