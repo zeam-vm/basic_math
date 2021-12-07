d6b4c50c5662a437eca931a59cd729b5b7540d20
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-07 11:02:00 +0900

    install ex_doc and dialyxir

--- mix.exs
+++ mix.exs
@@ -24,6 +24,8 @@ defmodule BasicMath.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.26", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false}
     ]
   end
 end
