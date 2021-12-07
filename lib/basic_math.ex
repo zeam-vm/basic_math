973c07b6ab689a23cd7df7c813d469ec9c47790c
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-07 12:48:40 +0900

    delegate factorial to factorial_ets

--- lib/basic_math.ex
+++ lib/basic_math.ex
@@ -2,4 +2,7 @@ defmodule BasicMath do
   @moduledoc """
   BasicMath provides faster basic mathematic functions, including factorial.
   """
+
+  @spec factorial(non_neg_integer) :: pos_integer
+  defdelegate factorial(n), to: BasicMath.Factorial.FactorialEts
 end
